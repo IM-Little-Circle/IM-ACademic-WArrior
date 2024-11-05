6ea224590968e72981fed6a8345db6372d88f8ab
cyyellow <102402091+cyyellow@users.noreply.github.com>
2024-11-06 02:21:40 +0800

    added event header template
    to dos: create collision detection bool, generate random pos for event, how to print event at given position

---
+++ event.h
@@ -0,0 +1,17 @@
+// note: keep trivial function definitions in header file
+// define non-trivial (important) functions in main
+
+class Event {
+    private:
+        int x;
+        int y;
+    public:
+        Event();
+        
+};
+
+Event::Event() {
+    // temp, set random pos upon initialization
+    x = 5;
+    y = 5;
+}
