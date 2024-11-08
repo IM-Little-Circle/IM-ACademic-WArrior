90d31c07322a5c58c74a6effe6118a4241f7c68f
EricChiu147 <88593071+EricChiu147@users.noreply.github.com>
2024-11-08 18:56:46 +0800

    add chance1

--- project_groupX/event.h
+++ project_groupX/event.h
@@ -1,5 +1,14 @@
 // note: keep trivial function definitions in header file
 // define non-trivial (important) functions in main
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using std::string;
+using std::ifstream;
+using std::cin;
+using std::cout;
+
 
 class Event {
     private:
@@ -8,12 +17,28 @@ class Event {
         
     public:
         Event();
-        
+        char* readPlot();
 };
 
 Event::Event() {
     // temp, set random pos upon initialization
     x = 5;
     y = 5;
+}
+
+char* Event::readPlot()
+{
+    char* str = NULL;
+    ifstream in("chance1.txt");
+
+    //for(int i = 0; i < 9; i++)
+    //    in.ignore();
+    
+    in >> str;
+
+    if(in.fail())
+        return "Event file NOT FOUND";
+    else
+        return str;
 }
 
---
+++ project_groupX/testmain.cpp
@@ -0,0 +1,8 @@
+#include"event.h"
+
+int main()
+{
+    Event chance1;
+    char* str = chance1.readPlot();
+    cout << str;
+}
