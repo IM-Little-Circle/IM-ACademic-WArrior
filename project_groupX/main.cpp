5af609cbf97765b9e80bcabd97f35569fabf4cff
cyyellow <102402091+cyyellow@users.noreply.github.com>
2024-11-08 20:55:44 +0800

    added detectEvent function

--- project_groupX/main.cpp
+++ project_groupX/main.cpp
@@ -9,7 +9,7 @@
 #include <Windows.h>
 
 // below are user-made header files
-#include "event.h"
+//#include "event.h"
 //#include "../Windows_Only_Header_Files/windows.h" //still working on this...
 
 using namespace std;
@@ -43,18 +43,10 @@ int y = 7; //btw the pos is temp too
 // add things here later
 
 
-// BIG QUESTION: how do we read the generated pos/info for each event? 
-// -> how do we read all the current existing events
-// store them in a vector, then once it's triggered, delete the element? list? some other data structure?
-
-// info can be generated beforehand, or when event is triggered ig
-
-
-
 //// functions (for game loop) ////
 void readMap() {
     string line;
-    ifstream file("../map.txt");
+    ifstream file("../testEventMap.txt");
 
     while (getline(file, line)) {
         map.push_back(line);
@@ -64,73 +56,92 @@ void readMap() {
 // for updating camera screen
 // question: how should we print the guide (press i for smtn smtn), at the right or bottom?
 // to do here: figure out/design the guide!
-void printMaze (bool visited[][MAP_W], int x, int y) {
+void printMaze (bool visited[][MAP_W]) {
     for (int j = y-SCRN_HW; j <= y+SCRN_HW; j++) {
 
-        cout << string(50, ' ');
+        cout << string(50, ' '); //padding
 
         for (int i = x-SCRN_HW; i <= x+SCRN_HW; i++) {
 
-            if (i == x && j == y) {
-                cout << "@ "; // player marker
-                visited[j][i] = 1; // necessary? (see below at declaration)
-            }
-
-            else if (i == 18 && j == 18) cout << "E"; // temp, for ending game
-
-            else if (i < 0 || i > 19 || j < 0 || j > 19) {
+            if (i < 0 || i >= MAP_W || j < 0 || j >= MAP_H) {
                 cout << "  "; // screen edge padding
             }
 
-            else cout << map[j][i] << " ";
+            else if (i == x && j == y) {
+                cout << "@ "; // player marker
+                visited[j][i] = 1;
+            }
+
+            else if (visited[j][i]) cout << ". ";
+
+            else if (i == 18 && j == 18) cout << "E"; // temp, for ending game
+
+            else { // events and battles (and nothing)
+                if (map[j][i] == 'c' || map[j][i] == 'd') cout << ". "; // accidents
+                else cout << map[j][i] << " "; // others
+                //cout << map[j][i] << " ";
+                //cout << ". ";
+            }
 
         }
         
         cout << "\n";
     }
+    // for debugging: cout pos
+    cout << string(50, ' ') << x << " " << y << endl;
 }
 
 
 void moveCamera(int ch) {
 switch(ch) {
         case KEY_UP:
-            if (y-1 >= 0 && map[y-1][x] == '.') {
+            if (y-1 >= 0 && map[y-1][x] != '#') {
                 y-=1;
             }
             break;
         case KEY_DOWN:
-            if (y+1 < 20 && map[y+1][x] == '.') {
+            if (y+1 < MAP_H && map[y+1][x] != '#') {
                 y+=1;
             }
             break;
         case KEY_LEFT:
-            cout << left << endl;
-            if (x-1 >= 0 && map[y][x-1] == '.') {
+            if (x-1 >= 0 && map[y][x-1] != '#') {
                 x-=1;
             }
             break;
         case KEY_RIGHT:
-            if (x+1 < 20 && map[y][x+1] == '.') {
+            if (x+1 < MAP_W && map[y][x+1] != '#') {
                 x+=1;
             }
             break;
     }
 }
 
+void detectEvent() {
+    char c = map[y][x];
+    // replace cout below with functions to call
+    if (c == 'c' || c == 'C') cout << string(50, ' ') << "Chance!";
+    else if (c == 'd' || c == 'D') cout << string(50, ' ') << "Destiny!";
+    else if (c == 'B') cout << string(50, ' ') << "Battle!";
+    cout << endl;
+}
+
 
 //// main function //// 
 int main () {
     int ch; // for reading arrow key
-    bool end; // for game loop
-    bool visited[MAP_H][MAP_W] = {0}; // is this necessary? (recording trodden paths with ',')
-    Event chance;
+    bool end = 0; // for game loop
+    bool visited[MAP_H][MAP_W] = {0};
+
+
+    //Event chance;
 
     // test event
-    Event testevent;
+    //Event testevent;
 
     readMap();
 
-    printMaze(visited, x, y);
+    printMaze(visited);
 
     while (!end) {
         ch = 0;
@@ -141,7 +152,8 @@ int main () {
                 moveCamera(ch);
 
                 system("cls");
-                printMaze(visited, x, y);
+                printMaze(visited);
+                detectEvent();
                 if (x == 18 && y == 18) end = 1; // temp, for ending game
             }
         }
