83458e51b8760f0c537f00845244546d1945397b
yujuan0215 <109050984+yujuan0215@users.noreply.github.com>
2024-11-12 15:40:38 +0800

    update entity

--- project_groupX/entity.h
+++ project_groupX/entity.h
@@ -1,20 +1,40 @@
 /*
 放一些暫時想到的數值和可能用到的函式
 */
-
+#include <iostream>
+using namespace std;
 class Entity
 {
 	private:
 		int hpCurr, atk, def, hpMax;
-		double magicBuff, physicalBuff;
-		//double magicRes, physical_RES;
-		// a bag for skill
-		//vector<skill> Skill;
-	public:	
-		void attack();
-        void heal();
-		void use_skill();		
+		double socialBuff = 0, studyBuff = 0;
+		bool alive = true;
+		double mentalRes, physicalRes;
 
+	public:	
+		void socialAttack(Entity enemy);
+        void heal();
+		void death();
+		void updateStatus(int academic, int social, int emo);
 };
-//test2
-//test1
+
+void socialAttack(Entity enemy)
+{
+	int atkCurr = atk * (1 + magicBuff);
+	enemy.hpCurr -= atkCurr - enemy.def * (1 + mentalRes);
+	if (enemy.hpCurr <= 0)
+		enemy.death();
+}
+
+void death()
+{
+	alive = false;
+}
+
+void updateStatus(int academic, int social ,int emo)
+{
+	socialBuff = (social + emo) / 100;
+	studyBuff = (academic + emo) / 100;
+	mentalRes = emo / 100;
+}
+
