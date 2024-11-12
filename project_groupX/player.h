6e504e13ea7eee45e04b6a6546bb2192da8335ad
cyyellow <102402091+cyyellow@users.noreply.github.com>
2024-11-12 14:59:35 +0800

    added testEventa

--- project_groupX/player.h
+++ project_groupX/player.h
@@ -1,16 +1,25 @@
 // note: keep trivial function definitions in header file
 // define non-trivial (important) functions in main
+#include <iostream>
+using namespace std;
 
 class Player {
     private:
         int x;
         int y;
-        int academic;
-        int social;
-        int emo;
+        
+        // initialized for testing
+        int academic = 0; 
+        int social = 0;
+        int emo = 0;
     public:
         Player();
 
+        // below added for testing
+        void modifyAcademic(int change);
+        void modifySocial(int change);
+        void modifyEmo(int change);
+        void printStat();
         
 };
 
@@ -18,4 +27,24 @@ Player::Player() {
     // temp, set random pos upon initialization
     x = 5;
     y = 5;
+}
+
+
+void Player::modifyAcademic(int change) {
+    academic += change;
+}
+
+
+void Player::modifySocial(int change) {
+    social += change;
+}
+
+
+void Player::modifyEmo(int change) {
+    emo += change;
+}
+
+
+void Player::printStat(){
+    cout << "ACA " << academic << "\nSOC " << social << "\nEMO " << emo << endl;
 }
