cd675ff75bbe56b353217f35263282ec7dde769f
EricChiu147 <88593071+EricChiu147@users.noreply.github.com>
2024-11-21 05:10:28 +0800

    add auto create /build function

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 ::g++ source_code/main.cpp -o build/game.exe -Wall -std=c++17 -Iinclude
+if not exist "build" mkdir build
 g++ source_code/main.cpp source_code/entity.cpp include/conio.c -o build/game.exe -std=c++17 -Iinclude -D__linux__
