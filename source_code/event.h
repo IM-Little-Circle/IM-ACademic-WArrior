1682686a65dfc811bcda4b5be7adc15baf3528c9
EricChiu147 <88593071+EricChiu147@users.noreply.github.com>
2024-12-05 16:10:29 +0800

    Update event.h

--- source_code/event.h
+++ source_code/event.h
@@ -4,7 +4,7 @@
 #include "battle.h"
 using namespace std;
 
-const int CHANCE_CNT = 1; //number of chances, must >= 1        //2 // for testing: 1
+const int CHANCE_CNT = 16; //number of chances, must >= 1       
 const int DESTINY_CNT = 12; //number of destinies, must >= 1    //17 // for testing: 12
 
 void displayEvent(ifstream& inFile);
